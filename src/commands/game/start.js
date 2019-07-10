8cda478264106b4224d5a53fad94a5cc25667df2
Ztumpie <stumpie905509@gmail.com>
2019-07-10 15:36:45 +0800

    start command ready

---
+++ src/commands/game/start.js
@@ -0,0 +1,22 @@
+const { Command } = require("../../core/exports")
+
+class start extends Command {
+    constructor(client) {
+        super(client, {
+            name: "start",
+            group: "game",
+            memberName: "start",
+            description: "Use this command to start the game of The Werewolves of the Miller's Hollow."
+        })
+    }
+
+    run(message) {
+        var { participantsList } = require("../../../index")
+        const { assignRole } = require("../../core/exports")
+        if(participantsList.length < 2) return message.channel.send("You don't have enough players to play.")
+        assignRole(participantsList, message)
+        participantsList = []
+    }
+}
+
+module.exports = start
